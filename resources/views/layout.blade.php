9522a0d4b0202a7f864f5160b9903f89d60ef329
Luke Strickland <luke@axxim.net>
2020-05-24 06:45:59 -0400

    Adding Google Analytics

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -40,6 +40,18 @@
             }
 
         </style>
+
+        @if(config('services.google_analytics.id'))
+            <!-- Global site tag (gtag.js) - Google Analytics -->
+                <script async src="https://www.googletagmanager.com/gtag/js?id={{ config('services.google_analytics.id') }}"></script>
+                <script>
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', '{{ config('services.google_analytics.id') }}');
+                </script>
+            @endif
     </head>
     <body>
 
